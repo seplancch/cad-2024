581b9a6420c2ce4cca1260a74239c32b0d549bc4
Jonathan Bailon Segura <jonn59@gmail.com>
2024-03-12 16:44:16 -0600

    creamos componente de tabla

---
+++ resources/views/components/table.blade.php
@@ -0,0 +1,8 @@
+<table class="table-auto w-full divide-gray-200">
+    <thead class="text-xs text-gray-800  bg-gray-200 dark:bg-gray-700 dark:text-gray-500">
+            {{ $head }}
+    </thead>
+    <tbody class="bg-white divide-y divide-gray-200">
+        {{ $body }}
+    </tbody>
+</table>
