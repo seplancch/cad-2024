a9375e19d34aa6ade6bd9d1ef4e8a5a092cae605
Jonathan Bailon Segura <jonn59@gmail.com>
2024-03-17 20:39:03 -0600

    Ajustamos nombres de clases en los modelos

--- app/Models/Grupos.php
+++ app/Models/Grupos.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class grupos extends Model
+class Grupos extends Model
 {
     use HasFactory;
 }
--- app/Models/Semestres.php
+++ app/Models/Semestres.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class profesores extends Model
+class Semestres extends Model
 {
     use HasFactory;
 }
