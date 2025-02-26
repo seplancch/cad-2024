d7369e330b48a23e85f4c14bb58aadc560cd2869
Jonathan Bailon Segura <jonn59@gmail.com>
2025-02-26 16:35:27 -0600

    agregamos el manejo de transacciones para asegurar integridad de datos

--- app/Http/Controllers/CsvImportController.php
+++ app/Http/Controllers/CsvImportController.php
@@ -11,6 +11,7 @@ use App\Models\Profesor;
 use App\Models\User;
 use Carbon\Carbon;
 use DateTime;
+use DB;
 
 class CsvImportController extends Controller
 {
@@ -30,71 +31,65 @@ class CsvImportController extends Controller
         $csv->setHeaderOffset(0);
 
         foreach ($csv as $record) {
+            DB::beginTransaction();
 
-            if(! User::where('username', $record['rfc'])->exists()){
-                $usuario = User::create([
-                    'username' => $record['rfc'],
-                    'name' => $record['nombre']. ' ' . $record['paterno'] . ' ' . $record['materno'],
-                    'email' => $record['rfc'].'@example.com',
-                    'password' => bcrypt($record['ntrabajador']), // O puedes usar 'password' => Hash::make($record['rfc']),
-                    'tipo' => 'P'
-                ]);
+            try {
+                $profesorExists = Profesor::where('rfc', $record['rfc'])->exists();
 
-                $profesor = $usuario->profesor()->create([
-                    'numero_trabajador' => $record['ntrabajador'],
-                    'rfc' => $record['rfc'],
-                    'plantel_id' => 1,
-                    'turno' => $record['turno'],
-                    //'fecha_nacimiento' => Carbon::parse($record['nacimiento'])->format('Y-m-d'),
-                    'fecha_nacimiento' => '1990-01-01',
-                    'antiguedad' => isset($record['antiguedad']) ? $record['antiguedad'] : 'null',
-                    'sexo' => isset($record['sexo']) ? $record['sexo'] : 'null',    // Agrega aquí los demás campos...
-                ]);
+                if (!User::where('username', $record['rfc'])->exists()) {
+                    $usuario = User::create([
+                        'username' => $record['rfc'],
+                        'name' => $record['nombre'] . ' ' . $record['paterno'] . ' ' . $record['materno'],
+                        'email' => $record['rfc'] . '@example.com',
+                        'password' => bcrypt($record['ntrabajador']), // O puedes usar 'password' => Hash::make($record['rfc']),
+                        'tipo' => 'P'
+                    ]);
 
-                $profesor->grupo()->create([
-                    'nombre' => $record['grupo'],
-                    'seccion' => $record['seccion'],
-                    'plantel_id' => Plantel::getIdPlantel($record['plantel']),
-                    'asignatura_id' => Asignatura::getIdAsignatura($record['asignatura']),
-                    'periodo_id' => 1,
-                ]);
-            }elseif(! Profesor::where('rfc', $record['rfc'])->exists()){
-                $usuario = User::where('username', $record['rfc'])->first();
-                $usuario->profesor()->create([
-                    'numero_trabajador' => $record['ntrabajador'],
-                    'rfc' => $record['rfc'],
-                    'plantel_id' => 1,
-                    'turno' => $record['turno'],
-                    //'fecha_nacimiento' => Carbon::parse($record['nacimiento'])->format('Y-m-d'),
-                    'fecha_nacimiento' => '1990-01-01',
-                    'antiguedad' => isset($record['antiguedad']) ? $record['antiguedad'] : 'null',
-                    'sexo' => isset($record['sexo']) ? $record['sexo'] : 'null',
-                ]);
-            /*}elseif(! Grupo::where('profesor_id', Profesor::where('rfc', $record['rfc'])->first()->id)->exists()){
-                $profesor = Profesor::where('rfc', $record['rfc'])->first();
-                $profesor->grupo()->create([
-                    'nombre' => $record['grupo'],
-                    'seccion' => $record['seccion'],
-                    'plantel_id' => Plantel::getIdPlantel($record['plantel']),
-                    'asignatura_id' => Asignatura::getIdAsignatura($record['asignatura']),
-                    'periodo_id' => 1,
-                ]);
-            }else{
-                return back()->with('error', 'El profesor con RFC: ' . $record['rfc'] . ' ya existe.');
-            }*/
-        }else{
-            $profesor = Profesor::where('rfc', $record['rfc'])->first();
-                $profesor->grupo()->create([
-                    'nombre' => $record['grupo'],
-                    'seccion' => $record['seccion'],
-                    'plantel_id' => Plantel::getIdPlantel($record['plantel']),
-                    'asignatura_id' => Asignatura::getIdAsignatura($record['asignatura']),
-                    'periodo_id' => 1,
-                ]);
+                    $profesor = $usuario->profesor()->create([
+                        'numero_trabajador' => $record['ntrabajador'],
+                        'rfc' => $record['rfc'],
+                        'plantel_id' => 1,
+                        'turno' => $record['turno'],
+                        'fecha_nacimiento' => '1990-01-01',
+                        'antiguedad' => isset($record['antiguedad']) ? $record['antiguedad'] : 'null',
+                        'sexo' => isset($record['sexo']) ? $record['sexo'] : 'null',
+                    ]);
+
+                    $profesor->grupo()->create([
+                        'nombre' => $record['grupo'],
+                        'seccion' => $record['seccion'],
+                        'plantel_id' => Plantel::getIdPlantel($record['plantel']),
+                        'asignatura_id' => Asignatura::getIdAsignatura($record['asignatura']),
+                        'periodo_id' => 1,
+                    ]);
+                } elseif (!$profesorExists) {
+                    $usuario = User::where('username', $record['rfc'])->first();
+                    $usuario->profesor()->create([
+                        'numero_trabajador' => $record['ntrabajador'],
+                        'rfc' => $record['rfc'],
+                        'plantel_id' => 1,
+                        'turno' => $record['turno'],
+                        'fecha_nacimiento' => '1990-01-01',
+                        'antiguedad' => isset($record['antiguedad']) ? $record['antiguedad'] : 'null',
+                        'sexo' => isset($record['sexo']) ? $record['sexo'] : 'null',
+                    ]);
+                } else {
+                    $profesor = Profesor::where('rfc', $record['rfc'])->first();
+                    $profesor->grupo()->create([
+                        'nombre' => $record['grupo'],
+                        'seccion' => $record['seccion'],
+                        'plantel_id' => Plantel::getIdPlantel($record['plantel']),
+                        'asignatura_id' => Asignatura::getIdAsignatura($record['asignatura']),
+                        'periodo_id' => 1,
+                    ]);
+                }
+
+                DB::commit();
+            } catch (\Exception $e) {
+                DB::rollBack();
+                return back()->with('error', 'Ocurrió un error al importar los datos: ' . $e->getMessage());
+            }
         }
-
-
-    }
 
         return back()->with('success', 'Los alumnos se han importado correctamente.');
     }
