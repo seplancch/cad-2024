a41b3acb70ad371b5b40c759fe7806f2a53ac671
Jonathan Bailon Segura <jonn59@gmail.com>
2025-03-09 19:33:52 -0600

    realizamos ajustes en carga de profesores

--- app/Http/Controllers/CsvImport/ImportProfesoresController.php
+++ app/Http/Controllers/CsvImport/ImportProfesoresController.php
@@ -12,32 +12,61 @@ use App\Models\Profesor;
 use App\Models\User;
 use Carbon\Carbon;
 use DateTime;
-use DB;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\DB;
 
+/**
+ * Controller for importing profesores from a CSV file.
+ *
+ * @category CSVImport
+ * @package  App\Http\Controllers\CsvImport
+ * @author   Jonathan Bailon Segura <jonn59@gmail.com>
+ * @license  MIT License
+ * @link     http://example.com
+ */
 class ImportProfesoresController extends Controller
 {
+    /**
+     * Display the import profesores view.
+     *
+     * @return \Illuminate\View\View
+     */
     public function index()
     {
-        return view('import.index');
+        return view('import.profesores');
     }
 
+    /**
+     * Import profesores from a CSV file.
+     *
+     * @param \Illuminate\Http\Request $request The request object containing the CSV file.
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function import(Request $request)
     {
-        $request->validate([
-            'csv_file' => 'required|file|mimes:csv,txt',
-        ]);
 
-        $csv = Reader::createFromPath($request->file('csv_file')->getRealPath(), 'r');
+        $request->validate([
+            'archivo_csv' => 'required|file|mimes:csv',
+        ]);
+        Log::info('Inicia la importacion de profesores');
+
+        $csv = Reader::createFromPath($request->file('archivo_csv')->getRealPath(), 'r');
         $csv->setHeaderOffset(0);
 
-       // ini_set('max_execution_time', 18000);
+        //ini_set('max_execution_time', 18000);
 
         $startTime = time();
         $maxExecutionTime = ini_get('max_execution_time') - 5; // 5 segundos de margen
 
+        $conteoCarga = 0;
         foreach ($csv as $record) {
             if ((time() - $startTime) > $maxExecutionTime) {
-                return back()->with('error', 'El tiempo máximo de ejecución se ha superado.');
+                log::error('El tiempo máximo de ejecución se ha superado. <br />Solo se procesaron ' . $conteoCarga . ' registros.');
+                return back()->with(
+                    'error',
+                    'El tiempo máximo de ejecución se ha superado. Solo se procesaron ' . $conteoCarga . ' registros.'
+                );
             }
 
             DB::beginTransaction();
@@ -49,11 +78,23 @@ class ImportProfesoresController extends Controller
                 DB::rollBack();
                 return back()->with('error', 'Ocurrió un error al importar los datos: ' . $e->getMessage());
             }
+            $conteoCarga++;
         }
 
-        return back()->with('success', 'Los alumnos se han importado correctamente.');
+        Log::info('Se han importado ' . $conteoCarga . ' registros de profesores correctamente.');
+        return back()->with(
+            'success',
+            'Se han importado ' . $conteoCarga . ' registros de profesores correctamente.'
+        );
     }
 
+    /**
+     * Process a single record from the CSV file.
+     *
+     * @param array $record The record to process.
+     *
+     * @return void
+     */
     private function processRegistro($record)
     {
         $profesorExists = Profesor::where('rfc', $record['rfc'])->exists();
@@ -61,7 +102,7 @@ class ImportProfesoresController extends Controller
         if (!User::where('username', $record['rfc'])->exists()) {
             $usuario = $this->createUser($record);
             $profesor = $this->createProfesor($usuario, $record);
-            $this->createGrupoIfNotExists($profesor, $record);
+            $this->_createGrupoIfNotExists($profesor, $record);
         } elseif (!$profesorExists) {
             $usuario = User::where('username', $record['rfc'])->first();
             $profesor = $this->createProfesor($usuario, $record);
@@ -72,48 +113,79 @@ class ImportProfesoresController extends Controller
         }
     }
 
-    private function createUser($record)
+    /**
+     * Create a new user from the given record.
+     *
+     * @param array $record The record containing user information.
+     *
+     * @return \App\Models\User
+     */
+    private function _createUser($record)
     {
-        return User::create([
-            'username' => $record['rfc'],
-            'name' => $record['nombre'] . ' ' . $record['paterno'] . ' ' . $record['materno'],
-            'email' => $record['rfc'] . '@example.com',
-            'password' => bcrypt($record['ntrabajador']), // O puedes usar 'password' => Hash::make($record['rfc']),
-            'tipo' => 'P'
-        ]);
+        return User::create(
+            [
+                'username' => $record['rfc'],
+                'name' => $record['nombre'] . ' ' . $record['paterno'] . ' ' . $record['materno'],
+                'email' => $record['rfc'] . '@example.com',
+                'password' => bcrypt($record['ntrabajador']), // O puedes usar 'password' => Hash::make($record['rfc']),
+                'tipo' => 'P'
+            ]
+        );
     }
 
-    private function createProfesor($usuario, $record)
+    /**
+     * Create a new profesor from the given user and record.
+     *
+     * @param \App\Models\User $usuario The user model.
+     * @param array $record The record containing profesor information.
+     *
+     * @return \App\Models\Profesor
+     */
+    private function _createProfesor($usuario, $record)
     {
-        return $usuario->profesor()->create([
-            'numero_trabajador' => $record['ntrabajador'],
-            'rfc' => $record['rfc'],
-            'plantel_id' => 1,
-            'turno' => $record['turno'],
-            'fecha_nacimiento' => '1990-01-01',
-            'antiguedad' => !empty($record['antiguedad']) ? $record['antiguedad'] : 0,
-            'sexo' => !empty($record['sexo']) ? $record['sexo'] : 'M',
-        ]);
+        return $usuario->profesor()->create(
+            [
+                'numero_trabajador' => $record['ntrabajador'],
+                'rfc' => $record['rfc'],
+                'plantel_id' => 1,
+                'turno' => $record['turno'],
+                'fecha_nacimiento' => '1990-01-01',
+                'antiguedad' => !empty($record['antiguedad']) ? $record['antiguedad'] : 0,
+                'sexo' => !empty($record['sexo']) ? $record['sexo'] : 'M',
+            ]
+        );
     }
 
-    private function createGrupoIfNotExists($profesor, $record)
+    /**
+     * Create a new grupo if it does not exist.
+     *
+     * @param \App\Models\Profesor $profesor The profesor model.
+     * @param array $record The record containing grupo information.
+     *
+     * @return void
+     */
+    private function _createGrupoIfNotExists($profesor, $record)
     {
-        $grupoExists = Grupo::where([
-            ['nombre', '=', $record['grupo']],
-            ['seccion', '=', $record['seccion']],
-            ['plantel_id', '=', Plantel::getIdPlantel($record['plantel'])],
-            ['asignatura_id', '=', Asignatura::getIdAsignatura($record['asignatura'])],
-            ['periodo_id', '=', 1]
-        ])->exists();
+        $grupoExists = Grupo::where(
+            [
+                ['nombre', '=', $record['grupo']],
+                ['seccion', '=', $record['seccion']],
+                ['plantel_id', '=', Plantel::getIdPlantel($record['plantel'])],
+                ['asignatura_id', '=', Asignatura::getIdAsignatura($record['asignatura'])],
+                ['periodo_id', '=', 1]
+            ]
+        )->exists();
 
         if (!$grupoExists) {
-            $profesor->grupo()->create([
-                'nombre' => $record['grupo'],
-                'seccion' => $record['seccion'],
-                'plantel_id' => Plantel::getIdPlantel($record['plantel']),
-                'asignatura_id' => Asignatura::getIdAsignatura($record['asignatura']),
-                'periodo_id' => 1,
-            ]);
+            $profesor->grupo()->create(
+                [
+                    'nombre' => $record['grupo'],
+                    'seccion' => $record['seccion'],
+                    'plantel_id' => Plantel::getIdPlantel($record['plantel']),
+                    'asignatura_id' => Asignatura::getIdAsignatura($record['asignatura']),
+                    'periodo_id' => 1,
+                ]
+            );
         }
     }
 }
