11f0d4502c44487b2699be17f2449d56f69f47aa
Jonathan Bailon Segura <jonn59@gmail.com>
2025-05-03 11:24:19 -0600

    cargamos periodos pasados en seeders

--- database/seeders/PeriodoSeeder.php
+++ database/seeders/PeriodoSeeder.php
@@ -13,14 +13,65 @@ class PeriodoSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('periodos')->insert([
+        DB::table('periodos')->insert(
             [
-                'cuestionario_id' => '1',
-                'clave' => '2024-2',
-                'descripcion' => 'CAD 2024-2',
-                'created_at' => \Carbon\Carbon::now(),
-                'updated_at' => \Carbon\Carbon::now()
-            ],
-        ]);
+                [
+                    'cuestionario_id' => '1',
+                    'clave' => '2017-2',
+                    'descripcion' => 'CAD 2024-2',
+                    'created_at' => \Carbon\Carbon::now(),
+                    'updated_at' => \Carbon\Carbon::now()
+                ],
+                [
+                    'cuestionario_id' => '1',
+                    'clave' => '2018-2',
+                    'descripcion' => 'CAD 2018-2',
+                    'created_at' => \Carbon\Carbon::now(),
+                    'updated_at' => \Carbon\Carbon::now()
+                ],
+                [
+                    'cuestionario_id' => '1',
+                    'clave' => '2019-2',
+                    'descripcion' => 'CAD 2019-2',
+                    'created_at' => \Carbon\Carbon::now(),
+                    'updated_at' => \Carbon\Carbon::now()
+                ],
+                [
+                    'cuestionario_id' => '1',
+                    'clave' => '2020-2',
+                    'descripcion' => 'CAD 2020-2',
+                    'created_at' => \Carbon\Carbon::now(),
+                    'updated_at' => \Carbon\Carbon::now()
+                ],
+                [
+                    'cuestionario_id' => '1',
+                    'clave' => '2021-2',
+                    'descripcion' => 'CAD 2021-2',
+                    'created_at' => \Carbon\Carbon::now(),
+                    'updated_at' => \Carbon\Carbon::now()
+                ],
+                [
+                    'cuestionario_id' => '1',
+                    'clave' => '2022-2',
+                    'descripcion' => 'CAD 2022-2',
+                    'created_at' => \Carbon\Carbon::now(),
+                    'updated_at' => \Carbon\Carbon::now()
+                ],
+                [
+                    'cuestionario_id' => '1',
+                    'clave' => '2023-2',
+                    'descripcion' => 'CAD 2023-2',
+                    'created_at' => \Carbon\Carbon::now(),
+                    'updated_at' => \Carbon\Carbon::now()
+                ],
+                [
+                    'cuestionario_id' => '1',
+                    'clave' => '2024-2',
+                    'descripcion' => 'CAD 2024-2',
+                    'created_at' => \Carbon\Carbon::now(),
+                    'updated_at' => \Carbon\Carbon::now()
+                ],
+            ]
+        );
     }
 }
