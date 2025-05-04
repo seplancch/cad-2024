7b362898feb10465918fd3e7fcff7318255a66f6
Jonathan Bailon Segura <jonn59@gmail.com>
2025-05-04 11:23:46 -0600

    modificamos helper de periodo

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -4,34 +4,51 @@ namespace App\Helpers;
 
 use App\Models\Configuracion;
 use App\Models\User;
+use App\Models\Periodo;
 
-function compruebaEstadoInscripciones($userid)
-{
-    $estadoInscripciones = [];
-    $usuario = User::find($userid);
-    $periodoActual = obtienePeriodoActual();
-    $inscripciones = $usuario->inscripcion->where('periodo_id', $periodoActual);
-    $estadoInscripciones['numeroGrupos'] = count($inscripciones);
-    $estadoInscripciones['estado'] = 0;
+if (! function_exists('compruebaEstadoInscripciones')) {
+    function compruebaEstadoInscripciones($userid)
+    {
+        $estadoInscripciones = [];
+        $usuario = User::find($userid);
+        $periodoActual = obtienePeriodoActual();
+        $inscripciones = $usuario->inscripcion->where('periodo_id', $periodoActual);
+        $estadoInscripciones['numeroGrupos'] = count($inscripciones);
+        $estadoInscripciones['estado'] = 0;
 
 
-    $estadoInscripciones['completados'] = 0;
+        $estadoInscripciones['completados'] = 0;
 
-    foreach ($inscripciones as $inscripcion) {
-        if ($inscripcion->estado == 1) {
-            $estadoInscripciones['completados']++;
+        foreach ($inscripciones as $inscripcion) {
+            if ($inscripcion->estado == 1) {
+                $estadoInscripciones['completados']++;
+            }
         }
-    }
 
-    if ($estadoInscripciones['completados'] ==  $estadoInscripciones['numeroGrupos']) {
-        $estadoInscripciones['estado'] = 1;
-    }
+        if ($estadoInscripciones['completados'] ==  $estadoInscripciones['numeroGrupos']) {
+            $estadoInscripciones['estado'] = 1;
+        }
 
-    return (object) $estadoInscripciones;
+        return (object) $estadoInscripciones;
+    }
 }
 
+if (! function_exists('obtieneIdPeriodoActual')) {
+    function obtieneIdPeriodoActual()
+    {
+        $config = Configuracion::where('nombre', 'PERIODO_ACTUAL')->first();
+        return $config ? $config->valor : null;
+    }
+}
 
-function obtienePeriodoActual()
-{
-    return Configuracion::find(1)->periodo->id;
+if (! function_exists('obtienePeriodoActual')) {
+    function obtienePeriodoActual()
+    {
+        $periodoId = obtieneIdPeriodoActual();
+        if ($periodoId) {
+            $periodo = Periodo::find($periodoId);
+            return $periodo ? $periodo->nombre : null;
+        }
+        return null;
+    }
 }
