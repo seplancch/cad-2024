60fe7ab8a5860a04301bd4b3dc5a4a0b4f2004ba
Jonathan Bailon Segura <jonn59@gmail.com>
2025-05-12 00:54:21 -0600

    aumentamos tiempo para carga de alumnos

--- app/Http/Controllers/CsvImport/ImportAlumnosController.php
+++ app/Http/Controllers/CsvImport/ImportAlumnosController.php
@@ -61,7 +61,7 @@ class ImportAlumnosController extends Controller
         $csv = Reader::createFromPath($request->file('archivo_csv')->getRealPath(), 'r');
         $csv->setHeaderOffset(0);
 
-        //ini_set('max_execution_time', 18000);
+        ini_set('max_execution_time', 0);
 
         $startTime = time();
         $maxExecutionTime = ini_get('max_execution_time') - 5; // 5 segundos de margen
