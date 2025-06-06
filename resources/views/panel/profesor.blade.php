3d45a021a35e73075436135043ec198d35783015
Jonathan Bailon Segura <jonn59@gmail.com>
2025-06-05 20:33:03 -0600

    ajustamos container

--- resources/views/panel/profesor.blade.php
+++ resources/views/panel/profesor.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="min-h-screen flex flex-col justify-center items-center bg-green-50">
-    <div class="w-full max-w-3xl p-8 bg-white rounded shadow-md mt-10">
+    <div class="w-full max-w-7xl p-8 bg-white rounded shadow-md mt-10">
         <h2 class="text-2xl font-bold text-center text-green-700 mb-6">Panel de Profesor</h2>
         <livewire:grupos-profesor />
     </div>
